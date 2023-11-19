1589311d0f1c36490c49be0628f542d42e9b83c3
unknown <amirghhastam@gmail.com>
2023-11-19 16:50:38 +0330

    responive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,21 @@ import { IoIosArrowBack } from 'react-icons/io'
 import { FaInstagram } from 'react-icons/fa'
 import { FaLinkedinIn } from 'react-icons/fa6'
 import { BsTwitterX } from 'react-icons/bs'
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { RiMenu4Fill } from 'react-icons/ri'
 
 import { Link } from 'react-router-dom'
 
 export default function Navbar () {
   const [isShowMenu, setIsShowMenu] = useState(false)
 
-  const showNavbar = event => {
-    setIsShowMenu(event.target.checked)
-  }
-
   return (
     <div className='container m-auto lg:px-20 px-12 max-[500px]:px-0'>
-      <nav className='flex justify-around items-center w-full bg-slate-800 text-white py-8 rounded-b-full'>
+      <nav className='flex justify-around items-center w-full bg-slate-800 text-white py-8 rounded-b-2xl'>
+        <RiMenu4Fill
+          onClick={() => setIsShowMenu(true)}
+          className='text-5xl hidden max-[768px]:block'
+        />
         <h3 className='logo text-lg'>
           <Link to='/'>BoundlessBook</Link>
         </h3>
@@ -39,28 +41,28 @@ export default function Navbar () {
             </Link>
           </li>
         </ul>
-        <label
-          onClick={() => showNavbar(event)}
-          className='burger max-[768px]:block min-[768px]:hidden relative w-9 h-6 bg-transparent cursor-pointer'
+        <Link
+          to='/login'
+          className='smky-btn3 relative hover:text-[#fff] py-2 px-6 after:absolute after:h-1 after:hover:h-[200%] transition-all duration-500 hover:transition-all hover:duration-500 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden z-20 after:z-[-20] after:bg-gray-400	after:rounded-t-full after:w-full after:bottom-0 after:left-0 text-white'
         >
-          <input type='checkbox' id='burger' />
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-        <Link className='login  max-[768px]:hidden' to='/login'>
           ورود / ثبت نام
         </Link>
       </nav>
-
+      {/* navbar mobile size */}
       <nav
         className={`nav-responsive overflow-y-auto rounded-bl-2xl hidden	text-white px-5 transition-all max-[768px]:${
           isShowMenu ? 'flex' : 'hidden'
-        } flex-col bg-gray-700 w-8/12 h-3/4 fixed top-0 right-0`}
+        } flex-col bg-gray-700 w-7/12 h-3/4 fixed top-0 right-0`}
       >
-        <h3 className='logo text-lg mt-6'>
-          <Link to='/'>BoundlessBook</Link>
-        </h3>
+        <div className='flex items-center justify-between mt-5 max-[400px]:flex-col-reverse'>
+          <h3 className='logo text-lg'>
+            <Link to='/'>BoundlessBook</Link>
+          </h3>
+          <RiCloseCircleLine
+            className=' text-5xl cursor-pointer mb-2'
+            onClick={() => setIsShowMenu(false)}
+          />
+        </div>
         <ul className='nav-lists-mobile mt-6'>
           <li className='nav-list-mobile py-2'>
             <Link to='/' className='nav-link-mobile'>
@@ -95,14 +97,14 @@ export default function Navbar () {
             </Link>
           </li>
         </ul>
-        <ul className='media-container flex justify-around mt-6'>
-          <li className='media border-2 border-white border-solid p-4 text-2xl rounded-full max-[400px]:p-2'>
+        <ul className='media-container flex justify-around m-6'>
+          <li className='media border-2 border-white border-solid p-3 text-2xl rounded-full max-[400px]:p-2 mx-1'>
             <FaInstagram />
           </li>
-          <li className='media border-2 border-white border-solid p-4 text-2xl rounded-full max-[400px]:p-2'>
+          <li className='media border-2 border-white border-solid p-3 text-2xl rounded-full max-[400px]:p-2 mx-1'>
             <FaLinkedinIn />
           </li>
-          <li className='media border-2 border-white border-solid p-4 text-2xl rounded-full max-[400px]:p-2'>
+          <li className='media border-2 border-white border-solid p-3 text-2xl rounded-full max-[400px]:p-2 mx-1'>
             <BsTwitterX />
           </li>
         </ul>
