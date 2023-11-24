1cf00a42d6095ea0550cce8f90c2d69298c8438e
unknown <amirghhastam@gmail.com>
2023-11-24 14:03:38 +0330

    Write component title

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Category.css'
+import ComponentTitle from '../ComponentTitle/ComponentTitle'
+
+export default function Category () {
+  return (
+    <div className='category my-8 text-center'>
+      <ComponentTitle title='دسته بندی کتاب ها' />
+
+
+    </div>
+  )
+}
