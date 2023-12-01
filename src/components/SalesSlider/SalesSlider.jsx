7d719b8fe4406f5636c0644acb6ef9949d8c678d
unknown <amirghhastam@gmail.com>
2023-12-01 12:14:52 +0330

    fix bug for slider

--- src/components/SalesSlider/SalesSlider.jsx
+++ src/components/SalesSlider/SalesSlider.jsx
@@ -8,7 +8,7 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 
 // import required modules
-import { Pagination } from 'swiper/modules'
+import { EffectFade, Pagination } from 'swiper/modules'
 import { Link } from 'react-router-dom'
 
 export default function SalesSlider () {
@@ -23,15 +23,15 @@ export default function SalesSlider () {
         }}
         breakpoints={{
           640: {
-            slidesPerView: 1,
+            slidesPerView: 2,
             spaceBetween: 20
           },
           768: {
-            slidesPerView: 2,
+            slidesPerView: 3,
             spaceBetween: 40
           },
           1024: {
-            slidesPerView: 3,
+            slidesPerView: 4,
             spaceBetween: 20
           }
         }}
