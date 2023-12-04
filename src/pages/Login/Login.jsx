dd445f336d50ab43c71d50b13a1cd81bffd508ab
unknown <amirghhastam@gmail.com>
2023-12-04 20:29:05 +0330

    write glass style and bg-image

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './Login.css'
-export default function Login() {
+export default function Login () {
   return (
-    <div>Login</div>
+    <div className="login-page bg-[url('/src/assets/images/bookcas.jpg')] bg-center bg-cover w-full h-screen overflow-hidden">
+      <div className='form-container w-[480px] h-[70vh] mx-auto my-24 bg-[#ffffff2b] rounded-3xl shadow-md backdrop-blur overflow-hidden'>
+        <form>
+          <label></label>
+        </form>
+      </div>
+    </div>
   )
 }
