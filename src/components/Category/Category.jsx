6f7a620511fe186bc44d666a77278de490ccf013
unknown <amirghhastam@gmail.com>
2023-12-08 16:02:43 +0330

    fix rsponsive bug

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -7,7 +7,7 @@ export default function Category () {
   return (
     <div className='container category my-8 text-center mx-auto'>
       <ComponentTitle title='دسته بندی کتاب ها' />
-      <div className='categories grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 auto-rows-auto mx-auto w-full'>
+      <div className='categories grid max-[400px]:grid-cols-1 max-[640px]:grid-cols-2 max-[640px]:gap-3 sm:grid-cols-3 sm:gap-5 lg:grid-cols-4 xl:grid-cols-5 auto-rows-auto mx-auto w-full'>
         <CategoryCard categoryName='جنایی' />
         <CategoryCard categoryName='معامایی' />
         <CategoryCard categoryName='تاریخی' />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -56,7 +56,7 @@ export default function Navbar () {
         </ul>
         <Link
           to='/login'
-          className='smky-btn3 relative hover:text-[#fff] py-2 px-6 after:absolute after:h-1 after:hover:h-[200%] transition-all duration-500 hover:transition-all hover:duration-500 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden z-20 after:z-[-20] after:bg-gray-400	after:rounded-t-full after:w-full after:bottom-0 after:left-0 text-white'
+          className='smky-btn3 relative hover:text-[#fff] py-2 px-6 after:absolute after:h-1 after:hover:h-[200%] transition-all duration-500 hover:transition-all hover:duration-500 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden z-20 after:z-[-20] after:bg-gray-400	after:rounded-t-full after:w-full after:bottom-0 after:left-0 text-white max-[300px]:px-1'
         >
           ورود / ثبت نام
         </Link>
