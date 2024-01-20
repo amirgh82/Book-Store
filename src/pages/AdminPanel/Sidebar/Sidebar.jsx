37f0a5d2b929e37e2afcdb3a608329f59d61b0cb
unknown <amirghhastam@gmail.com>
2024-01-20 17:30:59 +0330

    Add admin infoi

--- src/pages/AdminPanel/Sidebar/Sidebar.jsx
+++ src/pages/AdminPanel/Sidebar/Sidebar.jsx
@@ -3,9 +3,13 @@ import './Sidebar.css'
 import { Link } from 'react-router-dom'
 export default function Sidebar () {
   return (
-    <div className='sidebar-container text-teal-50 h-screen bg-slate-600'>
+    <div className='sidebar-container py-6 pr-3 text-teal-50 h-screen bg-slate-600'>
+      <div className='admin-info border-y-2 border-gray-500 border-solid py-3 flex items-center justify-around'>
+        <img src='/src/assets/images/me.jpg' alt='admin photo' className=' w-8 h-8 rounded-full' />
+        <h3 className='admin-name'>امیررضا قربانی</h3>
+      </div>
       <div className='sidebar-menu'>
-        <ul className='sidebar-lists py-6 pr-3 w-full'>
+        <ul className='sidebar-lists w-full'>
           <li className='mt-3 hover:bg-slate-500 w-full p-2 block rounded-lg'>
             <Link to='' className='sidebar-link'>
               صفحه اصلی
