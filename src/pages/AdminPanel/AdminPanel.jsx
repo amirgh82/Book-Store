4cb2a119417850e5249c8216e3d37dc1d8a44521
unknown <amirghhastam@gmail.com>
2024-01-20 13:01:53 +0330

    The basic structure of the admin panel

---
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AdminPanel.css'
+import { Outlet } from 'react-router-dom'
+export default function AdminPanel() {
+  return (
+    <div id='content'>
+        
+    </div>
+  )
+}
