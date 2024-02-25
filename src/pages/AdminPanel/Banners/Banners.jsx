927ccd51314b380691afaccbe8c9b94284a67300
unknown <amirghhastam@gmail.com>
2024-02-25 19:49:37 +0330

    Add new route in sidebar

---
+++ src/pages/AdminPanel/Banners/Banners.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Banners.css'
+export default function Banners() {
+  return (
+    <div>Banners</div>
+  )
+}
---
+++ src/pages/AdminPanel/Comments/Comments.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Comments.css'
+
+export default function Comments() {
+  return (
+    <div>Comments</div>
+  )
+}
